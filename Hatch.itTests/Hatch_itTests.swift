b2e41d218743b63759370fe862fb6a958188566a
Stephen Thomas <stephen@nbp-84-177.nbp.ruw.rutgers.edu>
2017-09-13 13:18:18 -0400

    Initial Commit

---
+++ Hatch.itTests/Hatch_itTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hatch_itTests.swift
+//  Hatch.itTests
+//
+//  Created by Stephen Thomas on 9/13/17.
+//  Copyright © 2017 Hatch Inc. All rights reserved.
+//
+
+import XCTest
+@testable import Hatch_it
+
+class Hatch_itTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
